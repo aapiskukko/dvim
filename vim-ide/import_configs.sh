a35b74166eadfa79793cb74bfed710d768eda5aa
Aatu Koivisto <aatukoivi@gmail.com>
2019-07-10 08:50:29 +0300

    Updates

---
+++ vim-ide/import_configs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BASE_DIR=.dvim
+CONF_DIR=configs
+
+cp $HOME/$BASE_DIR/coc-settings.json $CONF_DIR
+cp $HOME/$BASE_DIR/dvimrc $CONF_DIR
+cp $HOME/$BASE_DIR/pylintrc $CONF_DIR
+cp $HOME/$BASE_DIR/pycodestyle $CONF_DIR
